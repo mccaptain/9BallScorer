b81537358a9c42fa0807074d5138c84919935b55
mm <mm@pop-os.lan>
2026-07-05 15:44:51 -0700

    dist build for gh-pages

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE = 'apa-9ball-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cached) => cached || fetch(e.request))
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
+  );
+});
